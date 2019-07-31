b6a0f2ea0a45a4ef3d2080bbe999fad07eaae1be
Johnson Joseph <jhonsonj88@gmail.com>
2019-07-31 17:26:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const driver = {};
 function updateDriverWithKeyAndValue(driver, key, value){
   return Object.assign({},driver, {[name]: 'value'});
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    return Object.assign(driver, {[key]: value});
+}
+
+function deleteFromDriverByKey(driver, key){
+    return Object.assign({}, delete driver.key);
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key];
+    return driver;
+}
